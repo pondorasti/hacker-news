17291c6ce9405fa6b3a8003a13d8ad855fc26ee2
Alexandru Turcanu <pondorasti@gmail.com>
2022-11-13 13:35:08 -0800

    feat: inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import "./globals.css"
+import { Inter } from "@next/font/google"
+import clsx from "clsx"
+
+const inter = Inter({ variable: "--font-inter", display: "swap" })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="bg-black">
+    <html lang="en" className={clsx("bg-black", inter.variable)}>
       <head />
       <body>
         <main className="mx-auto flex max-w-2xl flex-col items-center justify-center justify-items-stretch bg-white py-16 px-4 ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">

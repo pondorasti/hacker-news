6f33e0ef46a36d4bf43d21a5d0c23eefb8a7da37
Alexandru Turcanu <pondorasti@gmail.com>
2022-11-13 10:47:58 -0800

    feat: tailwindcss class sorting

--- app/Post.tsx
+++ app/Post.tsx
@@ -9,7 +9,7 @@ function UpvoteIcon() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6 -rotate-90 text-gray-50"
+      className="h-6 w-6 -rotate-90 text-gray-300"
     >
       <path
         strokeLinecap="round"
@@ -20,11 +20,30 @@ function UpvoteIcon() {
   )
 }
 
+function CommentIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6 text-gray-200"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+      />
+    </svg>
+  )
+}
+
 export function PostSkeleton() {
   /* https://delba.dev/blog/animated-loading-skeletons-with-tailwind */
 
   return (
-    <div className={clsx("space-y-5 rounded-2xl bg-gray-800 p-4 h-18 mt-4 w-full")}>
+    <div className={clsx("h-18 mt-4 flex w-full flex-row space-x-2 rounded-2xl bg-gray-800 p-4")}>
       <div className="h-3 w-3/5 rounded-lg bg-gray-700"></div>
       <div className="h-3 w-4/5 rounded-lg bg-gray-600"></div>
     </div>
@@ -37,14 +56,13 @@ export async function Post({ id }: { id: number }) {
   if (!post) return null
 
   return (
-    <div className={clsx("rounded-2xl bg-gray-800 p-4 h-18 flex flex-row mt-4 w-full space-x-2")}>
-      <div className="flex flex-col items-center mt-0.5">
+    <div className={clsx("h-18 mt-4 flex w-full flex-row space-x-2 rounded-2xl bg-gray-800 p-4")}>
+      <div className="mt-0.5 flex flex-col items-center">
         <UpvoteIcon />
-        <span className="text-gray-50 text-xs">{post.score}</span>
+        <span className="text-xs text-gray-50">{post.score}</span>
       </div>
       <div className="flex flex-col space-y-5">
         <h2 className="text-lg font-medium text-gray-50">{post.title}</h2>
-        <div className="h-3 w-4/5 rounded-lg bg-gray-600"></div>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="bg-gray-900">
       <head />
       <body>
-        <main className="flex flex-col justify-center items-center max-w-xl mx-auto mt-8">
-          <h1 className="text-4xl font-medium text-gray-50 mb-8">Hacker News</h1>
-          {/* <div className="flex flex-col space-y-4 mt-8 w-full justify-items-stretch"> */}
+        <main className="mx-auto mt-8 flex max-w-xl flex-col items-center justify-center">
+          <h1 className="mb-8 text-4xl font-medium text-gray-50">Hacker News</h1>
           {children}
         </main>
       </body>
